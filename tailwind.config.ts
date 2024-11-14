1adac0d8e7ce7d00ed7776f7cbf6927f3385a2c7
Tsvetomir Dimitrov <ts.dimitrov93@gmail.com>
2024-11-14 20:35:24 +0200

    adding background and properties

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,29 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "clr-bg": "#1f1f38",
+        "clr-bg-variant": "#2c2c6c",
+        "clr-primary": "#4db5ff",
+        "clr-primary-variant": "rgba(77,181,255,0.4)",
+        "clr-white": "#fff",
+        "clr-light": "rgba(255,255,255,0.6)",
+      },
+      spacing: {
+        "container-lg": "75%",
+        "container-md": "86%",
+        "container-sm": "90%",
+      },
+      lineHeight: {
+        relaxed: "1.7",
+      },
+      transitionProperty: {
+        custom: "all 400ms ease",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      backgroundImage: {
+        'texture': "url('/images/bg-texture.png')",
       },
     },
   },
